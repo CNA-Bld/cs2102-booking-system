e9d8d64a48cc57c2f2e6a742c329e76bccf0a5af
CNA_Bld <cna.bld@gmail.com>
2014-11-01 21:33:47 +0800

    修复 IVLE login callback 地址

--- const.py
+++ const.py
@@ -1,4 +1,10 @@
-APP_PATH = 'http://localhost:8080/'
+import os
+
+if os.environ['SERVER_SOFTWARE'].startswith('Development'):
+    APP_PATH = 'http://localhost:8080'
+else:
+    APP_PATH = 'http://booking.softstar.org/'
+
 SESSION_PREFIX = 'session_'
 
 IVLE_API_KEY = "rZ43ZYiyDPeEHlzz40zJS"
